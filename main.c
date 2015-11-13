ca258d6453349070d0bce19c39700d558c04dbd5
hjbyt <sopu00@gmail.com>
2015-11-13 23:02:17 +0200

    simplified

--- main.c
+++ main.c
@@ -9,22 +9,6 @@
  * Declarations
  */
 
-typedef enum {
-    OP_ADD,
-    OP_SUBTRACT,
-    OP_RANGE_SUM
-} Operation;
-
-
-typedef int Bool;
-#define TRUE 1
-#define FALSE 0
-
-typedef int (*OperationFunction)(int a, int b);
-
-Bool getOperation(Operation *operation, OperationFunction *operation_function);
-int add(int a, int b);
-int subtract(int a, int b);
 int rangeSum(int a, int b);
 
 /*
@@ -32,16 +16,18 @@ int rangeSum(int a, int b);
  */
 
 int main(void) {
+    int scan_count;
+    char operation;
     int first_number;
     int second_number;
-    Operation operation;
-    OperationFunction function;
-    Bool success;
-    int scan_count;
     int result;
 
-    success = getOperation(&operation, &function);
-    if (!success) {
+    printf("Please choose an operation (+/-/$):\n");
+    scan_count = scanf("%c", &operation);
+    if (scan_count != 1
+        || (operation != '+'
+            && operation != '-'
+            && operation != '$')) {
         printf("Invalid Operation\n");
         return EXIT_FAILURE;
     }
@@ -60,68 +46,33 @@ int main(void) {
         return EXIT_FAILURE;
     }
 
-    if (operation == OP_RANGE_SUM && first_number > second_number) {
-        printf("The Result is: Not Valid\n");
-        return EXIT_FAILURE;
+    switch (operation) {
+        case '+':
+            result = first_number + second_number;
+            break;
+        case '-':
+            result = first_number - second_number;
+            break;
+        case '$':
+            if (first_number > second_number) {
+                printf("The Result is: Not Valid\n");
+                return EXIT_FAILURE;
+            }
+            result = rangeSum(first_number, second_number);
+            break;
+        default:
+            /* Flow shouldn't reach here */
+            return EXIT_FAILURE;
     }
 
-    result = function(first_number, second_number);
     printf("The Result is: %d\n", result);
 
     return EXIT_SUCCESS;
 }
 
 /*
- * Get operation from user.
- * operation - the operation the user chose
- * operation_function - function corresponding to the operation the user chose.
- * return True iff a valid operation was entered.
- */
-Bool getOperation(Operation *operation, OperationFunction *operation_function) {
-    char c;
-    int scan_count;
-
-    printf("Please choose an operation (+/-/$):\n");
-    scan_count = scanf("%c", &c);
-    if (scan_count != 1) {
-        return FALSE;
-    }
-
-    switch (c) {
-        case '+':
-            *operation = OP_ADD;
-            *operation_function = add;
-            break;
-        case '-':
-            *operation = OP_SUBTRACT;
-            *operation_function = subtract;
-            break;
-        case '$':
-            *operation = OP_RANGE_SUM;
-            *operation_function = rangeSum;
-            break;
-        default:
-            return FALSE;
-    }
-    return TRUE;
-}
-
-/*
- * Add operation
- */
-int add(int a, int b) {
-    return a + b;
-}
-
-/*
- * Subtract operation
- */
-int subtract(int a, int b) {
-    return a - b;
-}
-
-/*
- * Range sum operation
+ * Return the sum of all integers in the range [a, b].
+ * Preconditions: b >= a
  */
 int rangeSum(int a, int b) {
     return (a + b) * (b - a + 1) / 2;
